27b7767f04791d017a30234d4b6a6b7687ec15dd
Kowalski Dragon <kowalski7cc@users.noreply.github.com>
2022-10-25 20:52:24 +0200

    Quick redirect to program

--- gatsby-node.js
+++ gatsby-node.js
@@ -16,6 +16,31 @@ exports.createPages = ({ actions }) => {
     isPermanent: true,
     force: true,
   })
+
+  // TODO: Improve me
+  createRedirect({
+    fromPath: 'https://linuxdaymilano.org/2022',
+    toPath: 'https://linuxdaymilano.org/schedule?year=2022',
+    redirectInBrowser: true,
+    isPermanent: true,
+    force: true,
+  })
+
+  createRedirect({
+    fromPath: 'https://linuxdaymilano.org/2019',
+    toPath: 'https://linuxdaymilano.org/schedule?year=2019',
+    redirectInBrowser: true,
+    isPermanent: true,
+    force: true,
+  })
+
+  createRedirect({
+    fromPath: 'https://linuxdaymilano.org/2018',
+    toPath: 'https://linuxdaymilano.org/schedule?year=2018',
+    redirectInBrowser: true,
+    isPermanent: true,
+    force: true,
+  })
 }
 exports.createSchemaCustomization = ({ actions }) => {
   const { createTypes } = actions
