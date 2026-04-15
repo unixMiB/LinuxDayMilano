f588b967b0a7a98a73a5b166a5070aacd9b94c3f
tiziodcaio <d.bass05@proton.me>
2026-04-16 00:06:36 +0200

    chore: add some astro config Redirect and sitemap

--- astro.config.js
+++ astro.config.js
@@ -3,14 +3,25 @@ import react from "@astrojs/react";
 import Icons from "unplugin-icons/vite";
 import IconsAstro from "unplugin-icons/vite";
 
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
   // ...
-  integrations: [react(), IconsAstro({ compiler: "astro" })],
+  integrations: [react(), sitemap(), IconsAstro({ compiler: "astro" })],
   vite: {
     plugins: [
       Icons({
         compiler: "jsx",
       }),
     ],
+  },
+  redirects: {
+    "/2018": "/schedule?year=2018",
+    "/2019": "/schedule?year=2019",
+    "/2022": "/schedule?year=2022",
+    "/2023": "/schedule?year=2023",
+    "/2024": "/schedule?year=2024",
+    "/2025": "/schedule?year=2025",
+    "/2026": "/schedule?year=2026",
   },
 });
